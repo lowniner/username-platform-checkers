65f2cf2069dd7f2a66290095771a8273c5daadca
such <70591293+lowniner@users.noreply.github.com>
2020-12-12 20:46:20 -0300

    Update core.js

--- core.js
+++ core.js
@@ -6,7 +6,9 @@ const questions = require('questions');
 const SteamApi = require("steamapi");
 const apiuser = new SteamApi(config.steamapikey);
 const MojangAPI = require('mojang-api');
-const fs = require('fs')
+const fs = require('fs');
+const sleep = require('system-sleep');
+
 
 
 function start() {
@@ -17,6 +19,11 @@ console.log("\x1b[0m",`
 ╚════██║██║   ██║██║     ██╔══██║    ╚██╗  ╚═══██╗
 ███████║╚██████╔╝╚██████╗██║  ██║     ╚██╗██████╔╝
 ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝      ╚═╝╚═════╝`);
+console.log('----------------------------------------------------------------------')
+console.log('Multiple platforms username checker made by such#1111')
+console.log('All rights reserved - Astro - 2020')
+console.log('Gr33tz: Forever - Magic')
+console.log('----------------------------------------------------------------------')
 
 function clear() {
     console.clear();
@@ -24,8 +31,8 @@ function clear() {
 
 
 
-setTimeout(clear, 1500);
-setTimeout(asktype, 1500);
+setTimeout(clear, 2300);
+setTimeout(asktype, 2300);
 }
 
 start();
@@ -33,7 +40,7 @@ start();
 var logger = fs.createWriteStream('log.txt', {
     flags: 'a'
   })
-  
+
 
 function sc(user) {
     request(`https://soundcloud.com/${user}`, function(error, response, body) {
@@ -45,6 +52,7 @@ function sc(user) {
             console.log("\x1b[31m",`${user} taken.`);
         }
     })
+    sleep(500);
 }
 
 
@@ -58,6 +66,7 @@ function github(user) {
             console.log("\x1b[31m",`${user} taken.`);
         }
     })
+    sleep(500);
 }
 function cracked(user) {
     request(`https://cracked.to/${user}`, function (error, response, body){
@@ -69,6 +78,7 @@ function cracked(user) {
             console.log("\x1b[31m",`${user} taken.`);
         }
     })
+    sleep(500);
 }
 
 function welove(user){
@@ -81,6 +91,7 @@ function welove(user){
             console.log("\x1b[31m",`${user} taken.`);
         }
     })
+    sleep(500);
 }
 
 function codea(user) {
@@ -93,6 +104,7 @@ function codea(user) {
             console.log("\x1b[31m",`${user} taken.`);
         }
     })
+    sleep(500);
 }
 
 function steam(user) {
@@ -103,6 +115,7 @@ function steam(user) {
         console.log('\x1b[32m',`${user} not taken.`)
         logger.write(`${user} OPEN | STEAM\n`)
     });
+    sleep(500);
 }
 
 function lol(user) {
@@ -115,6 +128,7 @@ function lol(user) {
             logger.write(`${user} OPEN | LOL\n`)
         }
     })
+    sleep(500);
 }
 
 function minecraft(user) {
@@ -127,10 +141,24 @@ function minecraft(user) {
         else
             console.log("\x1b[31m",`${user} taken.`);
     });
+    sleep(500);
+}
+
+function tiblar(user) {
+    request(`https://tiblar.com/${user}`, function(error, response, body){ 
+        if (response.statusCode == 200) {
+            console.log("\x1b[31m",`${user} taken.`)
+        }
+        if (response.statusCode == 404) {
+            console.log('\x1b[32m',`${user} not taken.`)
+            logger.write(`${user} OPEN | Tib\n`)
+        }
+    })
+    sleep(500);
 }
 
 function asktype() {
-    console.log('[1] Minecraft \n[2] LOL\n[3] Steam /ID \n[4] Soundcloud\n[5] GitHub\n[6] Cracked.to\n[7] We<3\n[8] CodeAcademy');
+    console.log('[1] Minecraft \n[2] LOL\n[3] Steam /ID \n[4] Soundcloud\n[5] GitHub\n[6] Cracked.to\n[7] We<3\n[8] CodeAcademy\n[9] Tiblar (New Social)');
     console.log(' ')
     questions.askOne({ info:'Type' }, function(result){
         if (result == 1) {
@@ -203,6 +231,16 @@ function asktype() {
                     codea(user_url);
                 })
             })
+        }
+        else if( result == 9) {
+            console.clear();
+            questions.askOne({ info:'File name (make sure to add .txt at the end)' }, function(result) {
+                lineReader.eachLine(result, function(line, last) {
+                    let user_url = line.replace(' ', '_');
+                    tiblar(user_url);
+                })
+            })
+            
         }
 
       })
